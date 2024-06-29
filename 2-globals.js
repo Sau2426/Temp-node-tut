6dd97d64b4c7db25abd998c9657219b8f6176167
Sau2426 <saurabhpardeshi8987@gmail.com>
2024-06-29 13:29:56 +0530

    first commit

---
+++ 2-globals.js
@@ -0,0 +1,13 @@
+// GLOBALS     - NO WINDOW !!!!!
+
+//dirname      - path to current directory
+// __filename  - file name
+//require      - function to use modules (CommonJs)
+//module       - info about current module(file)
+//process      - info about env where the program is execute
+
+
+console.log(__dirname)
+setInterval(()=>{
+console.log('hello')
+},1000)
